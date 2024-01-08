b1df4f33140a618ccc5716127d377a792ba73d75
Rifat-Shuvo <rhshuvo2812@gmail.com>
2024-01-08 16:06:59 +0600

    The delete funtionalities making

--- index.js
+++ index.js
@@ -78,7 +78,13 @@ async function run() {
             const result = await alltask.updateOne(filter, updateddoc)
             res.send(result)
         })
-
+        app.delete('/todo/:id', async(req,res)=>{
+            const id = req.params.id
+            console.log(id);
+            const query = {_id: new ObjectId(id)}
+            const result = await alltask.deleteOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
